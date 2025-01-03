38048b5bad32b9dae2d910868b8bb5bb575cc5c6
Imre Nagy <i.nagy23@outlook.com>
2025-01-03 15:48:47 +0100

    update script to support custom working directories
    additional argument checks
    count number of updated files

--- gps_updater.sh
+++ gps_updater.sh
@@ -1,30 +1,51 @@
 #!/bin/zsh
-
 #
 # GPS location updater using exiftool
 #
 
-process_arguments() {
-    while [ -n "$1" ]
-    do
-        case $1 in
-            -h|--help) echo "GPS location updater script.\nCall with long,lat locations as arguments to update location data for image files in the images subdirectory."; exit 1;;
-        esac
-        shift
-    done
-}
+#
+# Default values
+#
+IMAGE_FOLDER="$(pwd)/images"
 
-process_arguments "$@"
+while [[ "$#" -gt 0 ]]
+do case $1 in
+    -c|--gps) GPS_COORDINATES="$2"
+    shift;;
+    -f|--folder) IMAGE_FOLDER="$2"
+    shift;;
+    -h|--help)
+      echo "help text"
+      exit 0;
+    shift;;
+    *) echo "Unknown parameter passed: $1"
+    exit 1;;
+esac
+shift
+done
 
-IMAGE_FOLDER="images"
-if [ ! -d "$IMAGE_FOLDER" ]; then
-    mkdir $IMAGE_FOLDER
+#
+# Validations
+#
+if [[ -z "$GPS_COORDINATES" ]]; then
+    echo "Error: No GPS coordinates provided. Use --gps \"LAT,LONG\""
+    exit 1
 fi
 
+if [ ! -d "$IMAGE_FOLDER" ]; then
+    echo "Error: Working folder does not exist! Folder path: $IMAGE_FOLDER"
+    exit -1
+fi
+
+echo "Working directory: $IMAGE_FOLDER"
+
+#
 # Processing coordinates and getting altitude
-RAW_LONGITUDE_ARG=${1:?"missing arg 1 for LONGITUDE"}
-LATITUDE_DEC=${2:?"missing arg 2 for LATITUDE"}
-LONGITUDE_DEC="${RAW_LONGITUDE_ARG//,/}"
+#
+LONGITUDE_DEC=$(echo $GPS_COORDINATES | cut -d ',' -f 2 | xargs)
+LATITUDE_DEC=$(echo $GPS_COORDINATES | cut -d ',' -f 1 | xargs)
+
+echo "Parsed coordinates: $LONGITUDE_DEC, $LATITUDE_DEC"
 
 JSON=`curl -L -X GET "https://api.opentopodata.org/v1/test-dataset?locations=$LONGITUDE_DEC,$LATITUDE_DEC" --no-progress-meter`
 ALTITUDE=`jq -r -n --argjson data $JSON '$data.results[0].elevation'`
@@ -57,7 +78,10 @@ LON_REF=$(awk -v lon="$LONGITUDE_DEC" 'BEGIN {print (lon < 0) ? "W" : "E"}')
 # Enable nullglob to avoid errors if no files match
 setopt NULL_GLOB
 
+#
 # Loop through and add GPS data if the file is an image
+#
+UPDATED_FILES_COUNT=0
 for FILE in $IMAGE_FOLDER/*; do
   if [[ $(exiftool -T -FileType "$FILE") =~ ^(JPEG|PNG|TIFF|GIF|WEBP|HEIC)$ ]]; then
     exiftool -overwrite_original \
@@ -67,8 +91,15 @@ for FILE in $IMAGE_FOLDER/*; do
       -GPSLongitudeRef="$LON_REF" \
       -GPSAltitude="$ALTITUDE" \
       "$FILE"
+    
     echo "Added GPS data to $FILE"
+
+    ((UPDATED_FILES_COUNT++))
+
   else
     echo "$FILE is not a supported image file."
   fi
 done
+
+echo
+echo "$UPDATED_FILES_COUNT images updated"
